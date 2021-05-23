0137bf83107285b058796933d7d661e55c959f26
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-05-23 16:59:28 -0400

    seasons exercise done

--- src/index.js
+++ src/index.js
@@ -1,40 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-  // to get a response, we have to pass into 2 seperate function callbacks 
-  // first argument is a function callback that gets called anytime that this function or 
-  // position successfully gets the user's location (success callback), and the
-  // 2nd function is the failure callback, which is called when the getCurrentPosition function
-  // is unable to to determine the user's physical location. 
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner'; 
  
-
-
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { lat: null, errorMessage: '' }; 
-    
+  state = { lat: null, errorMessage: '' };
+  
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({ lat: position.coords.latitude }); 
-      }, 
-      err => {
-        this.setState({ errorMessage: err.message });
-      }
+      position => this.setState({ lat: position.coords.latitude }), 
+      err => this.setState({ errorMessage: err.message })    
     );
   }
 
-
-
-  render() {
+  renderContent () {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage} </div>
     }
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>; 
+      return <SeasonDisplay lat={this.state.lat}/>
     }
-    return <div>Loading!</div>; 
+    return <Spinner message="Please accept location request"/>; 
+  }
+    
+  
+  render() {
+    return (<div className="border red">{this.renderContent()}</div>
+    );  
   }
 }
 
